370605fbe22879556144865a4b93c55d16a925ac
Michael Shelemba <michael.shelemba@gmail.com>
2023-08-07 03:06:22 +0200

    Remove unnecessary error conditions

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -11,34 +11,18 @@ export class TasksService {
   constructor(private http: HttpClient) { }
 
   load(): Observable<Task[]> {
-    return this.http.get<Task[]>(environment.apiUrl + 'tasks').pipe(
-      catchError((error: HttpErrorResponse) => {
-        return throwError(error);
-      })
-    );
+    return this.http.get<Task[]>(environment.apiUrl + 'tasks')
   }
 
   delete(id: string) {
-    return this.http.delete(environment.apiUrl + 'tasks/' + id).pipe(
-      catchError((error: HttpErrorResponse) => {
-        return throwError(error);
-      })
-    );
+    return this.http.delete(environment.apiUrl + 'tasks/' + id)
   }
 
   create(task: Task): Observable<{ task: Task }> {
-    return this.http.post<{task: Task}>(environment.apiUrl + 'tasks', task).pipe(
-      catchError((error: HttpErrorResponse) => {
-        return throwError(error);
-      })
-    );
+    return this.http.post<{task: Task}>(environment.apiUrl + 'tasks', task)
   }
 
   update(task: Task): Observable<{ task: Task }> {
-    return this.http.put<{task: Task}>(environment.apiUrl + 'tasks/' + task._id, task).pipe(
-      catchError((error: HttpErrorResponse) => {
-        return throwError(error);
-      })
-    );
+    return this.http.put<{task: Task}>(environment.apiUrl + 'tasks/' + task._id + '132321', task)
   }
 }
